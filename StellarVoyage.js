01cf54ed2c911dd26d3e2f2057b78cb3346adc1a
Athary <129800892+AtharySultan@users.noreply.github.com>
2024-03-10 07:11:04 +0300

    Add files via upload

---
+++ StellarVoyage.js
@@ -0,0 +1,25 @@
+
+function sunChange(){
+    document.getElementById('infos').innerHTML = 'Sun is the star at the center of the Solar System. It is a nearly perfect ball of hot plasma, heated to incandescence by nuclear fusion reactions in its core. The Sun radiates this energy mainly as light, ultraviolet, and infrared radiation, and is the most important source of energy for life on Earth.';
+}
+function marsChange(){
+    document.getElementById('infos').innerHTML = 'Mars is the fourth planet and the furthest terrestrial planet from the Sun. The reddish color of its surface is due to finely grained iron(III) oxide dust in the soil, giving it the nickname "the Red Planet". Mars has a second smallest radius among the planets in the Solar System ';
+}
+function mercuryChange(){
+    document.getElementById('infos').innerHTML = 'Mercury is the first planet from the Sun and the smallest planet in the Solar System. It is a terrestrial planet with a heavily cratered surface due to the planet having no geological activity and an extremely tenuous atmosphere (called an exosphere).';
+}
+function neptuneChange(){
+    document.getElementById('infos').innerHTML = 'Neptune is the eighth planet from the Sun and the farthest known planet in the Solar System. It is the fourth-largest planet in the Solar System by diameter, the third-most-massive planet, and the densest giant planet.';
+}
+function saturnChange(){
+    document.getElementById('infos').innerHTML = 'Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It has only one-eighth the average density of Earth, but is over 95 times more massive.';
+}
+function earthChange(){
+    document.getElementById('infos').innerHTML = 'Earth is the third planet from the Sun and the only place known in the universe where life has originated and found habitability. While Earth may not contain the largest volumes of water in the Solar System, only Earth sustains liquid surface water.';
+}
+function uranusChange(){
+    document.getElementById('infos').innerHTML = 'Uranus is the seventh planet from the Sun and is a gaseous cyan ice giant. Most of Uranus is made out of water, ammonia, and methane in a supercritical phase of matter, which in astronomy is called ice or volatiles.';
+}
+function venusChange(){
+    document.getElementById('infos').innerHTML = 'Venus is the second planet from the Sun. It is a rocky planet and the only Solar System object with a size and mass close to that of Earth. It has an atmosphere, which is by far the thickest of any rocky body in the Solar System.';
+}
